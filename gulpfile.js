e8b79c65d17a5c9b8b0c382585e29f34cdc862d4
marshallluebke <marshallluebke@aol.com>
2016-06-08 17:40:34 -0500

    Compressed SCSS files

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
 	concat = require('gulp-concat'),
+	compass = require('gulp-compass'),
 	browserify = require('gulp-browserify');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -11,6 +12,8 @@ var jsSources = [
 	'components/scripts/tagline.js',
 	'components/scripts/template.js'
 ];
+
+var sassSources = ['components/sassSources/style.scss'];
 
 
 // creates tagline.js in the scripts folder using coffee and gulp
@@ -28,4 +31,15 @@ gulp.task('js', function(){
 		.pipe(concat('script.js'))
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
+});
+
+
+gulp.task('compass', function(){
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded'
+		}))
+		.pipe(gulp.dest('builds/development/css'))
 });
